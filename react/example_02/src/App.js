3b1058ee9e4c7b049dcc86336308d350b15df263
wesleymsqt <wesleymsqt@alu.ufc.br>
2025-09-01 09:41:01 -0300

    feat: using useState

--- react/example_02/src/App.js
+++ react/example_02/src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { use, useState } from "react";
 import Header from "./components/Header";
 import Corpo from "./components/Corpo";
 
 import './App.css';
 
 export default function App() {
+ const [num, setNum] = useState(0);
+
   return (
     <>
       <div className="container">
         <Header />
         <Corpo />
 
-        <p className="texto">lorem </p>
+        <p className="texto">Texto com css </p>
+        <p> Valor do states: {num} </p>
+        <button onClick={() => setNum(num + 1)}>Incrementar</button>
       </div>
     </>
   );
